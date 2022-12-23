09d808fc55a241e9838b9b36d9593185dff7d24c
Burak Çakır <burak2043@gmail.com>
2022-12-23 17:15:01 +0300

    type checking added

--- temeller/types/main.js
+++ temeller/types/main.js
@@ -12,5 +12,44 @@
  *  Unknown
  *  TYPE INREFERENCE
  */
+/* ilk değer ataması yapılmasa da :string kullanımı ile bir sonraki satırdaki atamada string methodları intellisense tarafından algılanabilir */
+// değişken tanımlaması yapıldıktan sonra ilk değer ataması yapılmazsa any olarak algınlanır
+var a;
+a = "any";
+/* string */
 var x;
 x = "burak";
+/* number */
+var age;
+age = 30;
+/* boolean */
+var bool;
+bool = false;
+/* array */
+var a3;
+a3 = [1, 2, 3, 4, "s", false];
+var a1;
+a1 = ["a", "b", "c"];
+var a2;
+a2 = [1, 2, 4];
+/* Enum */
+var Color;
+(function (Color) {
+    Color[Color["Purple"] = 0] = "Purple";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Aqua"] = 2] = "Aqua";
+})(Color || (Color = {}));
+;
+var bgColor = Color.Aqua;
+bgColor = Color.Purple;
+/* Tuple []*/
+var t;
+t = ["Not found", 404];
+/* Unknown */
+//type checking yaparken  any yerine unknown kullanılması daha mantıklıdır.
+var u;
+var un;
+un = "asdas";
+un = 12;
+un = false;
+var unk = false;
--- temeller/types/main.ts
+++ temeller/types/main.ts
@@ -15,5 +15,54 @@
 
 
 /* ilk değer ataması yapılmasa da :string kullanımı ile bir sonraki satırdaki atamada string methodları intellisense tarafından algılanabilir */
+
+// değişken tanımlaması yapıldıktan sonra ilk değer ataması yapılmazsa any olarak algınlanır
+let a;
+a = "any";
+
+
+/* string */
 let x : string;
 x = "burak";
+
+/* number */
+let age: number;
+age = 30;
+
+
+/* boolean */
+let bool: boolean;
+bool = false;
+
+
+/* array */
+let a3: any[];
+a3 = [1, 2, 3, 4, "s", false];
+let a1: string[];
+
+a1 = ["a", "b", "c"];
+
+let a2: number[];
+a2 = [1, 2, 4]
+
+
+/* Enum */
+enum Color { Purple, Red, Aqua };
+let bgColor = Color.Aqua;
+bgColor = Color.Purple;
+
+
+/* Tuple []*/
+let t: [string, number];
+t = ["Not found", 404];
+
+/* Unknown */
+//type checking yaparken  any yerine unknown kullanılması daha mantıklıdır.
+let u: any;
+
+let un: unknown;
+un = "asdas";
+un = 12;
+un = false;
+
+let unk = false;
