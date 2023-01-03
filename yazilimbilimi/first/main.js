5268e0f1f267c8def5daf6824b8387604880d5bf
Burak Çakır <burak2043@gmail.com>
2023-01-03 15:56:05 +0300

    yazilimbilimi started

---
+++ yazilimbilimi/first/main.js
@@ -0,0 +1,19 @@
+var a;
+var b;
+a = "lorem ipsum dolor sit amet";
+b = 1992;
+console.log(a);
+console.log("Hello World");
+console.log(b);
+var langs;
+langs = ["test", "deneme", "asd"];
+console.log(langs);
+var bools;
+bools = [true, false];
+console.log(bools);
+function addNumber(n1, n2) {
+    return n1 + n2;
+}
+var x = addNumber(1, 2);
+console.log(x);
+//17:55
---
+++ yazilimbilimi/first/main.ts
@@ -0,0 +1,22 @@
+let a: string;
+let b: number;
+a = "lorem ipsum dolor sit amet";
+b = 1992;
+console.log(a);
+console.log("Hello World");
+console.log(b);
+
+let langs: string[];
+langs = ["test", "deneme", "asd"];
+console.log(langs);
+let bools: Array<boolean>;
+bools = [true, false];
+console.log(bools);
+
+function addNumber(n1: number, n2: number) :number {
+    return n1 + n2;
+}
+let x = addNumber(1, 2);
+console.log(x);
+
+//17:55
