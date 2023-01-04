26f17081a670e5578d2ee7d22b5570781e9dd755
Burak Çakır <burak2043@gmail.com>
2023-01-04 16:03:36 +0300

    inheritance kavrami eklendi

--- yazilimbilimi/first/main.js
+++ yazilimbilimi/first/main.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var a;
 var b;
 a = "lorem ipsum dolor sit amet";
@@ -16,4 +31,48 @@ function addNumber(n1, n2) {
 }
 var x = addNumber(1, 2);
 console.log(x);
-//17:55
+/* add */
+function addNo(num1, num2) {
+    if (num2 === undefined) {
+        console.log(num1);
+    }
+    else {
+        console.log(num1, num2);
+    }
+}
+addNo(1);
+/* class olusturma islemi */
+var Person = /** @class */ (function () {
+    function Person(name, age, phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        console.log("Kişi oluşturuldu");
+    }
+    Person.prototype.showInfo = function () {
+        console.log("\n        \u0130sim : ".concat(this.name, "\n        age : ").concat(this.age, "\n        phone : ").concat(this.phone, "\n        "));
+    };
+    return Person;
+}());
+var person = new Person("Burak", 20, "1234");
+/* inheritance kavramı */
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, age, phone, salary) {
+        var _this = _super.call(this, name, age, phone) || this;
+        _this.salary = salary;
+        return _this;
+    }
+    /* üst class'ın methodunu manipule etme */
+    Employee.prototype.showInfo = function () {
+        _super.prototype.showInfo.call(this);
+        console.log("Maaş : " + this.salary);
+    };
+    Employee.prototype.changeDepartment = function () {
+        console.log("Departman değiştiriliyor");
+    };
+    return Employee;
+}(Person));
+var emp1 = new Employee("Ahmet", 21, "12343321", 1000);
+emp1.showInfo();
+emp1.changeDepartment();
--- yazilimbilimi/first/main.ts
+++ yazilimbilimi/first/main.ts
@@ -13,10 +13,68 @@ let bools: Array<boolean>;
 bools = [true, false];
 console.log(bools);
 
-function addNumber(n1: number, n2: number) :number {
+function addNumber(n1: number, n2 :number) :number {
     return n1 + n2;
 }
 let x = addNumber(1, 2);
 console.log(x);
 
-//17:55
+
+/* add */
+function addNo(num1: number, num2?: number): void {
+    if (num2 === undefined) {
+        console.log(num1);
+    } else {
+        console.log(num1,num2);
+    }
+}
+addNo(1)
+
+
+/* class olusturma islemi */
+
+class Person {
+    name: string;
+    age: number;
+    phone: string;
+    constructor(name: string, age: number, phone: string) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        console.log("Kişi oluşturuldu");
+    }
+    showInfo() {
+        console.log(`
+        İsim : ${this.name}
+        age : ${this.age}
+        phone : ${this.phone}
+        `);
+    }
+}
+
+
+let person = new Person("Burak", 20, "1234");
+
+/* inheritance kavramı */
+
+class Employee extends Person {
+    salary: number
+    
+    constructor(name:string,age:number,phone:string,salary:number) {
+        super(name, age, phone);
+        this.salary = salary;
+    }
+    /* üst class'ın methodunu manipule etme */
+    showInfo(){
+        super.showInfo();
+        console.log("Maaş : " + this.salary);
+        
+    }
+    changeDepartment() {
+        console.log("Departman değiştiriliyor");
+    }
+}
+
+let emp1 = new Employee("Ahmet", 21, "12343321", 1000);
+emp1.showInfo();
+emp1.changeDepartment();
